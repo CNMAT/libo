4ccfebf9ec393b7cc0e6665c06d2a5f7e7a46eaa
maccallum <john.m@ccallum.com>
2013-10-27 01:57:23 -0700

    took out old unused functions from original implementation

--- osc_match.c
+++ osc_match.c
@@ -24,12 +24,6 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #include <stdio.h>
 #include <inttypes.h>
 
-static int osc_match_star(const char *pattern, const char *address);
-static int osc_match_star_r(const char *pattern, const char *address);
-static int osc_match_single_char(const char *pattern, const char *address);
-static int osc_match_bracket(const char *pattern, const char *address);
-static int osc_match_curly_brace(const char *pattern, const char *address);
-
 static int osc_match_range(const char *pattern, const char *address);
 
 #define OSC_MATCH_RANGE_NOMATCH 1
@@ -185,129 +179,6 @@ int osc_match(const char *pattern, const char *address, int *pattern_offset, int
 	// unreachable
 }
 
-static int osc_match_star(const char *pattern, const char *address)
-{
-	const char *address_start = address;
-	const char *pattern_start = pattern;
-	int num_stars = 0;
-	if(*address == '\0') { return 0; }
-	while(*address != '/' && *address != '\0'){
-		address++;
-	}
-	while(*pattern != '/' && *pattern != '\0'){
-		if(*pattern == '*'){
-			num_stars++;
-		}
-		pattern++;
-	}
-	pattern--;
-	address--;
-	switch(num_stars){
-	case 1:
-		{
-			const char *pp = pattern, *aa = address;
-			while(*pp != '*'){
-				if(!(osc_match_single_char(pp, aa))){
-					return 0;
-				}
-				if(*pp == ']' || *pp == '}'){
-					while(*pp != '[' && *pp != '{'){
-						pp--;
-					}
-				}
-				pp--;
-				aa--;
-			}
-		}
-		break;
-	case 2:
-#if (OSC_MATCH_ENABLE_2STARS == 1)
-		{
-			const char *pp = pattern, *aa = address;
-			while(*pp != '*'){
-				if(!(osc_match_single_char(pp, aa))){
-					return 0;
-				}
-				if(*pp == ']' || *pp == '}'){
-					while(*pp != '[' && *pp != '{'){
-						pp--;
-					}
-				}
-				pp--;
-				aa--;
-			}
-			aa++; // we want to start one character forward to allow the star to match nothing
-			const char *star2 = pp;
-			const char *test = aa;
-			int i = 0;
-			while(test > address_start){
-				pp = star2 - 1;
-				aa = test - 1;
-				i++;
-				while(*pp != '*'){
-					if(!osc_match_single_char(pp, aa)){
-						break;
-					}
-					if(*pp == ']' || *pp == '}'){
-						while(*pp != '[' && *pp != '{'){
-							pp--;
-						}
-					}
-					pp--;
-					aa--;
-				}
-				if(pp == pattern_start){
-					return 1;
-				}
-				test--;
-			}
-			return 0;
-		}
-		break;
-#else
-		return 0;
-#endif
-	default:
-#if (OSC_MATCH_ENABLE_NSTARS == 1)
-		return osc_match_star_r(pattern_start, address_start);
-		break;
-#else
-		return 0;
-#endif
-	}
-	return 1;
-}
-
-#if (OSC_MATCH_ENABLE_NSTARS == 1)
-static int osc_match_star_r(const char *pattern, const char *address)
-{
-	if(*address == '/' || *address == '\0'){
-		if(*pattern == '/' || *pattern == '\0' || (*pattern == '*' && ((*(pattern + 1) == '/') || *(pattern + 1) == '\0'))){
-			return 1;
-		}else{
-			return 0;
-		}
-	}
-	if(*pattern == '*'){
-		if(osc_match_star_r(pattern + 1, address)){
-			return 1;
-		}else{
-			return osc_match_star_r(pattern, address + 1);
-		}
-	}else{
-		if(!osc_match_single_char(pattern, address)){
-			return 0;
-		}
-		if(*pattern == '[' || *pattern == '{'){
-			while(*pattern != ']' && *pattern != '}'){
-				pattern++;
-			}
-		}
-		return osc_match_star_r(pattern + 1, address + 1);
-	}
-}
-#endif
-
 static int osc_match_range(const char *pattern, const char *address)
 {
 	const char *p = pattern;
@@ -337,32 +208,5 @@ static int osc_match_range(const char *pattern, const char *address)
 			break;
 		}
 	}
-	if(matched == val){
-		/*
-		while(*p != ']'){
-			p++;
-		}
-		*pattern = ++p;
-		*/
-	}
 	return matched;
-}
-
-static int osc_match_curly_brace(const char *pattern, const char *address)
-{
-	pattern++;
-	const char *ptr = pattern;
-	while(*ptr != '}' && *ptr != '\0' && *ptr != '/'){
-		while(*ptr != '}' && *ptr != '\0' && *ptr != '/' && *ptr != ','){
-			ptr++;
-		}
-		int n = ptr - pattern;
-		if(!strncmp(pattern, address, n)){
-			return n;
-		}else{
-			ptr++;
-			pattern = ptr;
-		}
-	}
-	return 0;
 }
