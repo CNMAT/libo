81eeb746802285e1e8914cc9a86b5f72dd41c428
maccallum <john.m@ccallum.com>
2013-10-28 12:58:12 -0700

    patched osc_match_test.c from pmatch

--- test/osc_match_test.c
+++ test/osc_match_test.c
@@ -167,17 +167,20 @@ int main(int argc, char **argv)
 	int num_osc_match_fails = 0;
 	int num_osckit_match_fails = 0;
 	/*
-	for(int i = 0; i < numtests; i++){
-		printf("%s %s\n", testar[i].pattern, testar[i].address);
-	}
+	  for(int i = 0; i < numtests; i++){
+	  printf("%s %s\n", testar[i].pattern, testar[i].address);
+	  }
 	*/
 	for(int i = 0; i < numtests; i++){
 		t_test t = testar[i];
 
 		int osc_match_po = 0, osc_match_ao = 0;
 		int osc_match_ret;
+		const int nnn = 10000;
 		OSC_PROFILE_TIMER_START(t1);
-		osc_match_ret = osc_match(t.pattern, t.address, &osc_match_po, &osc_match_ao);
+		for(int i = 0; i < nnn; i++){
+			osc_match_ret = osc_match(t.pattern, t.address, &osc_match_po, &osc_match_ao);
+		}
 		OSC_PROFILE_TIMER_STOP(t1);
 		OSC_PROFILE_TIMER_PRINTF(t1);
 
@@ -227,12 +230,14 @@ int main(int argc, char **argv)
 		int osckit_ao = 0;
 		osc_match_test_errno = 0;
 		OSC_PROFILE_TIMER_START(t2);
-		for(level = 0; level < num_pattern_parts && level < num_address_parts; level++){
-			if(!(osckit_match_ret *= PatternMatch(pattern_parts[level], address_parts[level]))){
-				break;
+		for(int i = 0; i < nnn; i++){
+			for(level = 0; level < num_pattern_parts && level < num_address_parts; level++){
+				if(!(osckit_match_ret *= PatternMatch(pattern_parts[level], address_parts[level]))){
+					break;
+				}
+				osckit_po += strlen(pattern_parts[level]);
+				osckit_ao += strlen(address_parts[level]);
 			}
-			osckit_po += strlen(pattern_parts[level]);
-			osckit_ao += strlen(address_parts[level]);
 		}
 		OSC_PROFILE_TIMER_STOP(t2);
 		OSC_PROFILE_TIMER_PRINTF(t2);
@@ -375,148 +380,148 @@ void oscwarning_cb(const char *st, ...)
 }
 
 Boolean PatternMatch (const char *  pattern, const char * test) {
-  theWholePattern = pattern;
+	theWholePattern = pattern;
   
-  if (pattern == 0 || pattern[0] == 0) {
-    return test[0] == 0;
-  } 
+	if (pattern == 0 || pattern[0] == 0) {
+		return test[0] == 0;
+	} 
   
-  if (test[0] == 0) {
-    if (pattern[0] == '*')
-      return PatternMatch (pattern+1,test);
-    else
-      return FALSE;
-  }
+	if (test[0] == 0) {
+		if (pattern[0] == '*')
+			return PatternMatch (pattern+1,test);
+		else
+			return FALSE;
+	}
 
-  switch (pattern[0]) {
-    case 0      : return test[0] == 0;
-    case '?'    : return PatternMatch (pattern + 1, test + 1);
-    case '*'    : 
-      if (PatternMatch (pattern+1, test)) {
-        return TRUE;
-      } else {
-	return PatternMatch (pattern, test+1);
-      }
-    case ']'    :
-    case '}'    :
-      OSCWarning("Spurious %c in pattern \".../%s/...\"",pattern[0], theWholePattern);
-      return FALSE;
-    case '['    :
-      return MatchBrackets (pattern,test);
-    case '{'    :
-      return MatchList (pattern,test);
-    case '\\'   :  
-      if (pattern[1] == 0) {
-	return test[0] == 0;
-      } else if (pattern[1] == test[0]) {
-	return PatternMatch (pattern+2,test+1);
-      } else {
-	return FALSE;
-      }
-    default     :
-      if (pattern[0] == test[0]) {
-	return PatternMatch (pattern+1,test+1);
-      } else {
-	return FALSE;
-      }
-  }
+	switch (pattern[0]) {
+	case 0      : return test[0] == 0;
+	case '?'    : return PatternMatch (pattern + 1, test + 1);
+	case '*'    : 
+		if (PatternMatch (pattern+1, test)) {
+			return TRUE;
+		} else {
+			return PatternMatch (pattern, test+1);
+		}
+	case ']'    :
+	case '}'    :
+		OSCWarning("Spurious %c in pattern \".../%s/...\"",pattern[0], theWholePattern);
+		return FALSE;
+	case '['    :
+		return MatchBrackets (pattern,test);
+	case '{'    :
+		return MatchList (pattern,test);
+	case '\\'   :  
+		if (pattern[1] == 0) {
+			return test[0] == 0;
+		} else if (pattern[1] == test[0]) {
+			return PatternMatch (pattern+2,test+1);
+		} else {
+			return FALSE;
+		}
+	default     :
+		if (pattern[0] == test[0]) {
+			return PatternMatch (pattern+1,test+1);
+		} else {
+			return FALSE;
+		}
+	}
 }
 
 
 /* we know that pattern[0] == '[' and test[0] != 0 */
 
 static Boolean MatchBrackets (const char *pattern, const char *test) {
-  Boolean result;
-  Boolean negated = FALSE;
-  const char *p = pattern;
+	Boolean result;
+	Boolean negated = FALSE;
+	const char *p = pattern;
 
-  if (pattern[1] == 0) {
-    OSCWarning("Unterminated [ in pattern \".../%s/...\"", theWholePattern);
-    return FALSE;
-  }
+	if (pattern[1] == 0) {
+		OSCWarning("Unterminated [ in pattern \".../%s/...\"", theWholePattern);
+		return FALSE;
+	}
 
-  if (pattern[1] == '!') {
-    negated = TRUE;
-    p++;
-  }
+	if (pattern[1] == '!') {
+		negated = TRUE;
+		p++;
+	}
 
-  while (*p != ']') {
-    if (*p == 0) {
-      OSCWarning("Unterminated [ in pattern \".../%s/...\"", theWholePattern);
-      return FALSE;
-    }
-    if (p[1] == '-' && p[2] != 0) {
-      if (test[0] >= p[0] && test[0] <= p[2]) {
-	result = !negated;
-	goto advance;
-      }
-    }
-    if (p[0] == test[0]) {
-      result = !negated;
-      goto advance;
-    }
-    p++;
-  }
+	while (*p != ']') {
+		if (*p == 0) {
+			OSCWarning("Unterminated [ in pattern \".../%s/...\"", theWholePattern);
+			return FALSE;
+		}
+		if (p[1] == '-' && p[2] != 0) {
+			if (test[0] >= p[0] && test[0] <= p[2]) {
+				result = !negated;
+				goto advance;
+			}
+		}
+		if (p[0] == test[0]) {
+			result = !negated;
+			goto advance;
+		}
+		p++;
+	}
 
-  result = negated;
+	result = negated;
 
-advance:
+ advance:
 
-  if (!result)
-    return FALSE;
+	if (!result)
+		return FALSE;
 
-  while (*p != ']') {
-    if (*p == 0) {
-      OSCWarning("Unterminated [ in pattern \".../%s/...\"", theWholePattern);
-      return FALSE;
-    }
-    p++;
-  }
+	while (*p != ']') {
+		if (*p == 0) {
+			OSCWarning("Unterminated [ in pattern \".../%s/...\"", theWholePattern);
+			return FALSE;
+		}
+		p++;
+	}
 
-  return PatternMatch (p+1,test+1);
+	return PatternMatch (p+1,test+1);
 }
 
 static Boolean MatchList (const char *pattern, const char *test) {
 
- const char *restOfPattern, *tp = test;
+	const char *restOfPattern, *tp = test;
 
 
- for(restOfPattern = pattern; *restOfPattern != '}'; restOfPattern++) {
-   if (*restOfPattern == 0) {
-     OSCWarning("Unterminated { in pattern \".../%s/...\"", theWholePattern);
-     return FALSE;
-   }
- }
+	for(restOfPattern = pattern; *restOfPattern != '}'; restOfPattern++) {
+		if (*restOfPattern == 0) {
+			OSCWarning("Unterminated { in pattern \".../%s/...\"", theWholePattern);
+			return FALSE;
+		}
+	}
 
- restOfPattern++; /* skip close curly brace */
+	restOfPattern++; /* skip close curly brace */
 
 
- pattern++; /* skip open curly brace */
+	pattern++; /* skip open curly brace */
 
- while (1) {
+	while (1) {
    
-   if (*pattern == ',') {
-     if (PatternMatch (restOfPattern, tp)) {
-       return TRUE;
-     } else {
-       tp = test;
-       ++pattern;
-     }
-   } else if (*pattern == '}') {
-     return PatternMatch (restOfPattern, tp);
-   } else if (*pattern == *tp) {
-     ++pattern;
-     ++tp;
-   } else {
-     tp = test;
-     while (*pattern != ',' && *pattern != '}') {
-       pattern++;
-     }
-     if (*pattern == ',') {
-       pattern++;
-     }
-   }
- }
+		if (*pattern == ',') {
+			if (PatternMatch (restOfPattern, tp)) {
+				return TRUE;
+			} else {
+				tp = test;
+				++pattern;
+			}
+		} else if (*pattern == '}') {
+			return PatternMatch (restOfPattern, tp);
+		} else if (*pattern == *tp) {
+			++pattern;
+			++tp;
+		} else {
+			tp = test;
+			while (*pattern != ',' && *pattern != '}') {
+				pattern++;
+			}
+			if (*pattern == ',') {
+				pattern++;
+			}
+		}
+	}
 
 }
 
